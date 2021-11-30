35699cb3ad3349a5ebbd01ab74649dcb7348a995
Sam @ Future Design <19208973+futuredesignUK@users.noreply.github.com>
2021-11-30 14:44:58 +0000

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "futuredesignUK"
 
     workspaces {
       name = "sentinel-example"
